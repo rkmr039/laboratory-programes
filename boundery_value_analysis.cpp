39c3e28dcb41ccec97c5f58d5415bf39fd27f969
Rishab Kumar <rkmr039@gmail.com>
2018-09-08 15:20:25 +0530

    Program for demonstration of Boundary Value Analysis
    AIM: Write a program to determine the nature of a quadratic equation. its input is triple of positive integers and values may be from interval [1,100] and perform Boundary value Analysis.

--- boundery_value_analysis.cpp
+++ boundery_value_analysis.cpp
@@ -5,9 +5,9 @@ int main()
 {
     int a,b,c;
     float x1,x2,d;
-    char status = 'y';
+    int status = 1;
     printf("================Boundary Value Analysis===========\n");
-    while(status == 'y')
+    while(status == 1)
     {
         printf("For Equation ax^2+bx+c = 0\n\n");
         printf("\nEnter a:");scanf("%d",&a);
@@ -17,16 +17,25 @@ int main()
         x1 = ((-b)+pow(d,0.5))/(2*a);
         x2 = ((-b)-pow(d,0.5))/(2*a);
 
-        printf("\nRoots are: %f and %f\n\n",x1,x2);
+        if(a<1 || a>100 || b<0 || b>100 || c<0 || c>100)
+	    {
+	    printf("\nEntered values should be within 1 to 100\n");
+	    }
+        else
+        {
+        cout<<"\nRoots are: "<<x1<<" and "<<x2<<"\n\n";
         if(d > 0)
             printf("Real and unequal roots\n");
         if(d == 0)
             printf("Real and same roots\n");
         else
             printf("imaginary roots\n");
-
-        printf("\nDo you want to continue...?[y/n]\n");
-        scanf("%c",&status);
+        }
+        t = clock() - t;
+        double time_taken = ((double)t)/CLOCKS_PER_SEC;
+        printf("took %f seconds to execute \n", time_taken);
+        printf("\nDo you want to continue...?[1/0]\n");
+        scanf("%d",&status);
     }
     return 0;
 }
